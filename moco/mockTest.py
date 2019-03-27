e47e43afeb487355437ebf51e714b73069fb6cd5
lidebao513 <690372397@qq.com>
2019-03-28 00:02:06 +0800

    22

--- moco/mockTest.py
+++ moco/mockTest.py
@@ -10,18 +10,18 @@ class MockTest(unittest.TestCase):
     def test_fail_send(self):
         fail_send=mock.Mock(return_value=404)
         client.send_request=fail_send
-        self.assertEqual(client.visit_baidu(),404)
+        self.assertEqual(client.visit_baidu(),fail_send())
     def test_success_send(self):
         success_send =mock.Mock(return_value=200)
         client.send_request=success_send
-        self.assertEqual(client.visit_baidu(),200)
+        self.assertEqual(client.visit_baidu(),success_send())
     def test_remove_success(self):
         remove_success=mock.Mock(return_value='success')
         self.r.rmdir=remove_success
-        self.assertEqual(self.r.exists_get_rmdir(),'success')
+        self.assertEqual(self.r.exists_get_rmdir(),remove_success())
     def test_remove_fail(self):
         remove_fail=mock.Mock(return_value='fail')
         self.r.rmdir=remove_fail
-        self.assertEqual(self.r.exists_get_rmdir(),'fail')
+        self.assertEqual(self.r.exists_get_rmdir(),remove_fail())
 if __name__ == '__main__':
     unittest.main(verbosity=2)
